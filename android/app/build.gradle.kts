587ef34e5a9bbf225dc3501a6eb6d6835b349581
Ricardo Morin <morinricardo@gmail.com>
2026-07-11 13:20:15 -0700

    Tweak android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,6 +27,11 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt")
+            )
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
